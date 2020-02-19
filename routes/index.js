b822807dfe2bf9342e2da1efbda66014d117c6f2
Bruno Maciel Duarte <brunomacielduarte32@gmail.com>
2020-02-18 22:38:24 -0300

    Salvando trabalho

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/produtos', function (req, res) {
+    res.json([
+            {
+                id: 1,
+                product_name: 'Sanduich',
+                value: 2.00
+            },
+            {
+                id: 1,
+                product_name: 'Ração de Gato',
+                value: 2.00
+            },
+            {
+                id: 1,
+                product_name: 'Salada de Biscoito',
+                value: 2.00
+            }
+        ],
+    )
 });
 
 module.exports = router;
