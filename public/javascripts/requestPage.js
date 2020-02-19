50574df6365272b58af0545ee1daa593c62498d9
Bruno Maciel Duarte <brunomacielduarte32@gmail.com>
2020-02-19 15:37:46 -0300

    Input Value Efect

--- public/javascripts/requestPage.js
+++ public/javascripts/requestPage.js
@@ -11,10 +11,51 @@ async function start() {
 function handleInputValue(event) {
     let value = event.target.value;
     value = value.replace('R$', '');
-
-    console.log(value.length)
+    value = value.replace(',', '');
+    value = parseInt(+value, 10);
+    value = value.toString();
 
     event.target.value = '';
+
+    var tecla = event.keyCode;
+    if (tecla > 65 && tecla < 90) {
+        return;
+    }
+
+    switch (value.length) {
+        case 1:
+            value = '0,0' + value;
+            break;
+        case 2:
+            value = '0,' + value;
+            break;
+        case 3:
+            value = value.split('');
+            value.map((vl, index) => {
+                if (index === 0) {
+                    value = vl + ',';
+                } else {
+                    value += vl;
+                }
+            });
+            break;
+        case 4:
+            value = value.split('');
+            value = value.slice(0, 2).toString().replace(',', '') + ',' +
+                value.slice(2, 4).toString().replace(',', '');
+            break;
+        case 5:
+            value = value.split('');
+            value = value.slice(0, 3).toString().replace(/[,]+/g, '') + ',' +
+                value.slice(3, 5).toString().replace(/[,]/g, '');
+            break;
+        case 6:
+            value = value.split('');
+            value = value.slice(0, 4).toString().replace(/[,]+/g, '') + ',' +
+                value.slice(4, 6).toString().replace(/[,]/g, '');
+            break;
+    }
+
     event.target.value = 'R$' + value;
 }
 
