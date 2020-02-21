07ac8225a97b8411d85ec67616f1bebe8971ddc8
Bruno Maciel Duarte <brunomacielduarte32@gmail.com>
2020-02-21 10:25:35 -0300

    salvando trabalho

--- src/services/PayPalService.js
+++ src/services/PayPalService.js
@@ -1,10 +1,5 @@
 const axios = require('axios');
-
-const acess = {
-    urlPayPal: 'https://api.sandbox.paypal.com',
-    username: 'AW533V1aK0mHQhlqcJ9bi6XuUzOBQTJYNW7ttrDSg2Y87HlNu7YQ_Pu1KhImqZc5vbf9s2XtCQ2U8ydz',
-    password: 'EFQ5i7d3rgxDNeRCTU2AihSsm7Dwgsb-HcKF1SK6uVVVM0CB265kgcWNRTC5xwpam9xPWh3vTPTyC4P1'
-};
+const {payPal} = require('../config/AcessServices');
 
 const api = axios.create({
     baseURL: acess.urlPayPal
@@ -22,10 +17,10 @@ class PayPalService {
             //Usando application/x-www-form-urlencoded format
             const params = new URLSearchParams();
             params.append('grant_type', 'client_credentials');
-            const {data} = await api.post(acess.urlPayPal + '/v1/oauth2/token', params, {
+            const {data} = await api.post(payPal.urlPayPal + '/v1/oauth2/token', params, {
                 auth: {
-                    username: acess.username,
-                    password: acess.password,
+                    username: payPal.username,
+                    password: payPal.password,
                 },
             });
 
