5af3c6bb1846915149ef29e1881c1effe1fce3a8
Bruno Maciel Duarte <brunomacielduarte32@gmail.com>
2020-02-27 15:54:03 -0300

    Update AcessServices.exemplo.js

--- src/config/AcessServices.exemplo.js
+++ src/config/AcessServices.exemplo.js
@@ -1,7 +1,7 @@
 module.exports = {
     payPal: {
-        urlPayPal: 'https://api.sandbox.paypal.com',
-        username: 'AW533V1aK0mHQhlqcJ9bi6XuUzOBQTJYNW7ttrDSg2Y87HlNu7YQ_Pu1KhImqZc5vbf9s2XtCQ2U8ydz',
-        password: 'EFQ5i7d3rgxDNeRCTU2AihSsm7Dwgsb-HcKF1SK6uVVVM0CB265kgcWNRTC5xwpam9xPWh3vTPTyC4P1'
+         urlPayPal: 'https://api.sandbox.paypal.com',
+         username: 'CLIENT_ID',
+         password: 'SECRET'
     }
 };
