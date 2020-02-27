f10c1dba1e3cc45f51c6d9d5142c7aceb70bc713
Bruno Maciel Duarte <brunomacielduarte32@gmail.com>
2020-02-27 17:57:48 -0300

    Create AcessServices.js

---
+++ src/config/AcessServices.js
@@ -0,0 +1,7 @@
+module.exports = {
+    payPal: {
+        urlPayPal: 'https://api.sandbox.paypal.com',
+        username: 'AW533V1aK0mHQhlqcJ9bi6XuUzOBQTJYNW7ttrDSg2Y87HlNu7YQ_Pu1KhImqZc5vbf9s2XtCQ2U8ydz',
+        password: 'EFJj5UaoTYCnPqnZmziM4v8xE_aDEULB0uKLnFLImWPiI2Veadh4kP4b4i1V5L2A9s4hOg9EROlhubze'
+    }
+};
